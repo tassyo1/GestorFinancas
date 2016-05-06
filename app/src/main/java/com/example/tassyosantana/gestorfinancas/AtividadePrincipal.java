bbd48b63c25086e343090d16b77bcf381b1c6c31
Tássyo Santana <tassyo.santana@hotmail.com>
2016-05-06 17:59:23 -0300

    Segunda Atividade

--- app/src/main/java/com/example/tassyosantana/gestorfinancas/AtividadePrincipal.java
+++ app/src/main/java/com/example/tassyosantana/gestorfinancas/AtividadePrincipal.java
@@ -1,7 +1,10 @@
 package com.example.tassyosantana.gestorfinancas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AtividadePrincipal extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class AtividadePrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_principal);
+
+
+
+
+        Button adicionaCategoria = (Button) findViewById(R.id.adicionaCategoria);
+        adicionaCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Activity2 = new Intent(AtividadePrincipal.this, SegundaAtividade.class);
+                startActivityForResult(Activity2, 1);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tassyosantana/gestorfinancas/SegundaAtividade.java
@@ -0,0 +1,13 @@
+package com.example.tassyosantana.gestorfinancas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaAtividade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_atividade);
+    }
+}
