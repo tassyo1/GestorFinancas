d2ae0b38243c22defbf07e5e1d61427a6244c213
Tássyo Santana <tassyo.santana@hotmail.com>
2016-05-07 16:16:39 -0300

    Terminado o layout(por hora...)

--- app/src/main/java/com/example/tassyosantana/gestorfinancas/AtividadePrincipal.java
+++ app/src/main/java/com/example/tassyosantana/gestorfinancas/AtividadePrincipal.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AtividadePrincipal extends AppCompatActivity {
 
@@ -14,9 +15,7 @@ public class AtividadePrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_atividade_principal);
 
 
-
-
-        Button adicionaCategoria = (Button) findViewById(R.id.adicionaCategoria);
+        ImageButton adicionaCategoria = (ImageButton) findViewById(R.id.adicionaCategoria);
         adicionaCategoria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
