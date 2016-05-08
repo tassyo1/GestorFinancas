4be5053b20b27f1a54338852f91d035b91aa7797
Tássyo Santana <tassyo.santana@hotmail.com>
2016-05-08 06:14:32 -0300

    Alterando a gridview deixando-a com apenas uma coluna e vários subitens

--- app/src/main/java/com/example/tassyosantana/gestorfinancas/AtividadePrincipal.java
+++ app/src/main/java/com/example/tassyosantana/gestorfinancas/AtividadePrincipal.java
@@ -1,20 +1,28 @@
 package com.example.tassyosantana.gestorfinancas;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.widget.TextView;
 import java.util.ArrayList;
 
 public class AtividadePrincipal extends AppCompatActivity {
+    GridView grid;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +43,39 @@ public class AtividadePrincipal extends AppCompatActivity {
 
         ArrayList <String> a = new ArrayList<String>();
         a.add("lanche");
-        a.add("-5,00");
+        a.add("-500000,00");
         a.add("30/04/2015");
-        a.add("2000,00");
-        a.add("Berlim");
-        a.add("Praga");
-        a.add("Moscou");
-        a.add("Amsterdan");
+        a.add("20000,00");
+        a.add("TV à cabo");
+        a.add("-150,00");
+        a.add("05/05/2016");
+        a.add("100000000,00");
+        grid = (GridView) findViewById(R.id.gridView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, a);
 
-        GridView grid = (GridView) findViewById(R.id.gridView);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, a){
+            public View getView(int position, View convertView, ViewGroup parent){
+                View view = super.getView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP,17);
+                tv.setGravity(Gravity.LEFT);
+
+
+                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                        AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.MATCH_PARENT
+                );
+                tv.setLayoutParams(lp);
+
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) tv.getLayoutParams();
+                params.height = getPixelsFromDPs(AtividadePrincipal.this,25);
+                params.width = getPixelsFromDPs(AtividadePrincipal.this, 150);
+                tv.setLayoutParams(params);
+
+                return tv;
+            }
+        };
+
         grid.setAdapter(adapter);
 
 
@@ -57,6 +86,12 @@ public class AtividadePrincipal extends AppCompatActivity {
                         ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
             }
         });
-
+    }
+    // Method for converting DP value to pixels
+    public static int getPixelsFromDPs(Activity activity, int dps){
+        Resources r = activity.getResources();
+        int  px = (int) (TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dps, r.getDisplayMetrics()));
+        return px;
     }
 }
