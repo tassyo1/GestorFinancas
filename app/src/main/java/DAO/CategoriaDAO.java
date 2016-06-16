5e3ac79b5694c463c4d04539a4d0afc1cfc53cda
Tássyo Santana <tassyo.santana@hotmail.com>
2016-06-16 18:20:31 -0300

    Alterado o DAO de categoria

--- app/src/main/java/DAO/CategoriaDAO.java
+++ app/src/main/java/DAO/CategoriaDAO.java
@@ -1,5 +1,6 @@
 package DAO;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,28 +9,33 @@ import java.util.ArrayList;
 import Model.Categoria;
 
 /**
- * Created by tassyosantana on 06/05/16.
+ * Created by tassyosantana on 16/06/16.
  */
 public class CategoriaDAO {
 
     private Banco banco;
+    PreparedStatement pstmt;
+    Statement stmt;
 
-    public CategoriaDAO(Context context){
-        banco = new Banco(context);
+    public CategoriaDAO() throws SQLException, ClassNotFoundException{
+        banco = new Banco();
+        stmt = (Statement) banco.getConn().createStatement();
     }
 
-    public String inserir(String tipo, String nome, Integer frequencia_id, String data, Float valor){
-        ContentValues valores;
+    public String inserir(String tipo, String nome, Integer frequencia_id, String data, Float valor)
+    throws ClassNotFoundException, SQLException{
+        pstmt = banco.getConn().prepareStatement("insert into categorias (tipo, nome, frequencia_id, " +
+                " data_agendada, valor) values (?,?,?,?,?");
         long resultado;
-        db = banco.getWritableDatabase();
-        valores = new ContentValues();
-        valores.put("tipo",tipo);
-        valores.put("nome",nome);
-        valores.put("frequencia_id",frequencia_id);
-        valores.put("data_agendada",data);
-        valores.put("valor",valor);
-        resultado = db.insert("categorias", null, valores);
-        db.close();
+
+        pstmt.setString(1, tipo);
+        pstmt.setString(2,nome);
+        pstmt.setString(3, frequencia_id.toString());
+        pstmt.setString(4,data);
+        pstmt.setString(5,valor.toString());
+
+        resultado = pstmt.executeUpdate();
+        banco.fecharConexao();
 
         if (resultado == -1)
             return "Erro ao inserir registro";
@@ -37,18 +43,22 @@ public class CategoriaDAO {
         return "Registro inserido com sucesso";
     }
 
-    public String atualizar(Integer id, String tipo, String nome, Integer frequencia_id, String data, Float valor ){
-        ContentValues valores;
+    public String atualizar(Integer id, String tipo, String nome, Integer frequencia_id, String data, Float valor )
+    throws ClassNotFoundException, SQLException{
+        pstmt = banco.getConn().prepareStatement("update categorias set tipo =? , set nome =? ," +
+                "set frequencia_id =? , set data_agendada =?, valor =? where id =?");
+
         long resultado;
-        db =banco.getWritableDatabase();
-        valores = new ContentValues();
-        valores.put("tipo",tipo);
-        valores.put("nome",nome);
-        valores.put("frequencia_id",frequencia_id);
-        valores.put("data_agendada",data);
-        valores.put("valor",valor);
-        resultado = db.update("categorias", valores, "id =" + id, null);
-        db.close();
+
+        pstmt.setString(1, tipo);
+        pstmt.setString(2, nome);
+        pstmt.setString(3, frequencia_id.toString());
+        pstmt.setString(4, data);
+        pstmt.setString(5, valor.toString());
+        pstmt.setString(6, id.toString());
+
+        resultado = pstmt.executeUpdate();
+        banco.fecharConexao();
 
         if (resultado == -1)
             return "Erro ao atualizar registro";
@@ -56,11 +66,10 @@ public class CategoriaDAO {
         return "Registro atualizado com sucesso";
     }
 
-    public String deletar(Integer id){
+    public String deletar(Integer id) throws SQLException{
         long resultado;
-        db =banco.getWritableDatabase();
-        resultado = db.delete("categorias", "id=" + id, null);
-        db.close();
+        resultado =stmt.executeUpdate("delete from categorias where id = "+id);
+        banco.fecharConexao();
 
         if (resultado == -1)
             return "Erro ao deletar registro";
@@ -68,20 +77,21 @@ public class CategoriaDAO {
         return "Registro deletado com sucesso";
     }
 
-    public boolean temMovimento(Integer id){
+    public boolean temMovimento(Integer id) throws SQLException{
         boolean tem;
         String query = "select categorias.* from categorias inner join movimentos on categorias.id = categoria_id where " +
                 "categorias.id = "+id +"  limit 1 ";
 
         String descricao ="";
 
-        db = banco.getReadableDatabase();
-        Cursor c = db.rawQuery(query,null);
+        ResultSet rs = stmt.executeQuery(query);
 
-        if (c.moveToFirst())
-            descricao = c.getString(c.getColumnIndex("nome"));
+        while (rs.next()){
+            descricao = rs.getString("nome");
+        }
 
-        db.close();
+        banco.fecharConexao();
+
 
         if (descricao.equals(""))
             tem = false;
@@ -91,58 +101,53 @@ public class CategoriaDAO {
         return tem;
     }
 
-    public ArrayList<Categoria> listaTodasCategorias() {
+    public ArrayList<Categoria> listaTodasCategorias() throws SQLException, ClassNotFoundException{
 
         String query = "select * from categorias order by nome";
 
-        db = banco.getReadableDatabase();
-        Cursor c = db.rawQuery(query,null);
+        ResultSet rs = stmt.executeQuery(query);
 
         ArrayList<Categoria> array = new ArrayList<Categoria>();
 
-        if (c.moveToFirst()){
-            do {
-                Categoria categoria = new Categoria();
-                categoria.setId(c.getInt(c.getColumnIndex("id")));
-                categoria.setNome(c.getString(c.getColumnIndex("nome")));
-                categoria.setDataAgendada(c.getString(c.getColumnIndex("data_agendada")));
-                categoria.setFrequencia_id(c.getInt(c.getColumnIndex("frequencia_id")));
-                categoria.setTipo(c.getString(c.getColumnIndex("tipo")));
-                categoria.setValor(c.getFloat(c.getColumnIndex("valor")));
+        while (rs.next()){
+            Categoria categoria = new Categoria();
+            categoria.setId(rs.getInt("id"));
+            categoria.setNome(rs.getString("nome"));
+            categoria.setDataAgendada(rs.getDate("data_agendada").toString());
+            categoria.setFrequencia_id(rs.getInt("frequencia_id"));
+            categoria.setTipo(rs.getString("tipo"));
+            categoria.setValor(rs.getFloat("valor"));
 
-                array.add(categoria);
-            }while(c.moveToNext());
+            array.add(categoria);
         }
-        db.close();
+
+        banco.fecharConexao();
 
         return array;
     }
 
-    public Categoria buscaPorNome(String nome){
-        String query =
-                "SELECT * FROM categorias where nome like '"+nome+"'";
-        db = banco.getReadableDatabase();
-        Cursor c = db.rawQuery(query,null);
+    public Categoria buscaPorNome(String nome) throws SQLException{
+        String query = "SELECT * FROM categorias where nome like '"+nome+"'";
+
+        ResultSet rs = stmt.executeQuery(query);
 
         Categoria categoria_model = new Categoria();
-        if (c.moveToFirst()) {
 
-            do {
-                categoria_model.setId(c.getInt(c.getColumnIndex("id")));
-                categoria_model.setNome(c.getString(c.getColumnIndex("nome")));
-                categoria_model.setDataAgendada(c.getString(c.getColumnIndex("data_agendada")));
-                categoria_model.setFrequencia_id(c.getInt(c.getColumnIndex("frequencia_id")));
-                categoria_model.setTipo(c.getString(c.getColumnIndex("tipo")));
-                categoria_model.setValor(c.getFloat(c.getColumnIndex("valor")));
-            }
-            while (c.moveToNext()) ;
+        while (rs.next()) {
+            categoria_model.setId(rs.getInt("id"));
+            categoria_model.setNome(rs.getString("nome"));
+            categoria_model.setDataAgendada(rs.getDate("data_agendada").toString());
+            categoria_model.setFrequencia_id(rs.getInt("frequencia_id"));
+            categoria_model.setTipo(rs.getString("tipo"));
+            categoria_model.setValor(rs.getFloat("valor"));
+
         }
-        db.close();
+        banco.fecharConexao();
 
         return categoria_model;
     }
     // categoria com nome já existente
-    public Boolean validaNomeCategoria(String nome, Integer id){
+    public Boolean validaNomeCategoria(String nome, Integer id) throws SQLException{
         Boolean valida;
         Categoria categoria = this.buscaPorNome(nome);
 
@@ -167,59 +172,54 @@ public class CategoriaDAO {
     }
 
 
-    public ArrayList<Categoria> buscaCategoriasEventuais() {
+    public ArrayList<Categoria> buscaCategoriasEventuais() throws SQLException {
 
         String query = "select categorias.* from categorias left join movimentos on categorias.id = categoria_id where categoria_id is null "+
         "and DATE (substr(data_agendada, 7, 4) || '-' || substr(data_agendada, 4, 2) || '-' || substr(data_agendada, 1, 2)) "+
                 "<= date('now') and frequencia_id = 1;";
 
-        db = banco.getReadableDatabase();
-        Cursor c = db.rawQuery(query,null);
+        ResultSet rs = stmt.executeQuery(query);
 
         ArrayList<Categoria> array = new ArrayList<Categoria>();
 
-        if (c.moveToFirst()){
-            do {
-                Categoria categoria = new Categoria();
-                categoria.setId(c.getInt(c.getColumnIndex("id")));
-                categoria.setNome(c.getString(c.getColumnIndex("nome")));
-                categoria.setDataAgendada(c.getString(c.getColumnIndex("data_agendada")));
-                categoria.setFrequencia_id(c.getInt(c.getColumnIndex("frequencia_id")));
-                categoria.setTipo(c.getString(c.getColumnIndex("tipo")));
-                categoria.setValor(c.getFloat(c.getColumnIndex("valor")));
+        while (rs.next()) {
+            Categoria categoria = new Categoria();
+            categoria.setId(rs.getInt("id"));
+            categoria.setNome(rs.getString("nome"));
+            categoria.setDataAgendada(rs.getDate("data_agendada").toString());
+            categoria.setFrequencia_id(rs.getInt("frequencia_id"));
+            categoria.setTipo(rs.getString("tipo"));
+            categoria.setValor(rs.getFloat("valor"));
 
-                array.add(categoria);
-            }while(c.moveToNext());
+            array.add(categoria);
         }
-        db.close();
+        banco.fecharConexao();
 
         return array;
     }
 
-    public ArrayList<Categoria> buscaCategoriasFrequentes(){
-        String query = "select categorias.* from categorias where frequencia_id <> 1 "+
+    public ArrayList<Categoria> buscaCategoriasFrequentes() throws SQLException {
+        String query = "select categorias.* from categorias where frequencia_id <> 1 " +
                 "and DATE (substr(data_agendada, 7, 4) || '-' || substr(data_agendada, 4, 2) || '-' || substr(data_agendada, 1, 2)) " +
                 "<= date('now');";
 
-        db = banco.getReadableDatabase();
-        Cursor c = db.rawQuery(query,null);
+        ResultSet rs = stmt.executeQuery(query);
 
         ArrayList<Categoria> array = new ArrayList<Categoria>();
 
-        if (c.moveToFirst()){
-            do {
-                Categoria categoria = new Categoria();
-                categoria.setId(c.getInt(c.getColumnIndex("id")));
-                categoria.setNome(c.getString(c.getColumnIndex("nome")));
-                categoria.setDataAgendada(c.getString(c.getColumnIndex("data_agendada")));
-                categoria.setFrequencia_id(c.getInt(c.getColumnIndex("frequencia_id")));
-                categoria.setTipo(c.getString(c.getColumnIndex("tipo")));
-                categoria.setValor(c.getFloat(c.getColumnIndex("valor")));
+        while (rs.next()) {
 
-                array.add(categoria);
-            }while(c.moveToNext());
+            Categoria categoria = new Categoria();
+            categoria.setId(rs.getInt("id"));
+            categoria.setNome(rs.getString("nome"));
+            categoria.setDataAgendada(rs.getDate("data_agendada").toString());
+            categoria.setFrequencia_id(rs.getInt("frequencia_id"));
+            categoria.setTipo(rs.getString("tipo"));
+            categoria.setValor(rs.getFloat("valor"));
+
+            array.add(categoria);
         }
-        db.close();
+        banco.fecharConexao();
 
         return array;
     }
